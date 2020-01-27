cc540a81a7efb61bce0118b9ef2b4d81e9475a3d
DongCheol <vyvydkf628@naver.com>
2020-01-27 23:31:39 +0900

    add client side input form

--- playground/5-es6-objects.js
+++ playground/5-es6-objects.js
@@ -28,7 +28,7 @@ const product = {
 // console.log(stock)
 // console.log(rating)
 
-const transaction = (type,{ label,stock})=>{
+const transaction = (type,{ label,stock}={})=>{
     console.log(type,label,stock)
 } 
 
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -53,16 +53,12 @@ app.get('/weather',(req,res)=>{
     else{
         geocode(req.query.address,(error,{latitude,longitude,location}={})=>{
             if (error){
-                return res.send({
-                    error
-                })
+                return res.send({error})
             }
         
             forecast(latitude,longitude,(error,forecastData)=>{
                 if(error){
-                    return res.send({
-                        error
-                    })
+                    return res.send({error})
                 }
                 res.send({
                     location,
